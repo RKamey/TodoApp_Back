31e5585693105713d60f0e6563ec31718a85c4a2
RKamey <ralonso@ucol.mx>
2025-03-27 01:08:33 -0600

    feat: update npm scripts for development and production builds

--- src/server.ts
+++ src/server.ts
@@ -9,5 +9,5 @@ const PORT = process.env.PORT || 4006;
 dotenv.config({ path: path.resolve(__dirname, '../.env')});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}!`);
 });
