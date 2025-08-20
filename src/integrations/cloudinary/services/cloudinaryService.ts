94f9542f2e7185b10c19e388c47ff974d1bd1020
alonsokamey <alonsokamey@gmail.com>
2025-08-19 22:45:31 -0600

    fix(cloudinary): comment out unused uploadTestService function and its logic

--- src/integrations/cloudinary/services/cloudinaryService.ts
+++ src/integrations/cloudinary/services/cloudinaryService.ts
@@ -1,18 +1,18 @@
 import type { Request, Response } from "express";
 
-export const uploadTestService = async (req: Request, res: Response) => {
-  if (!req.file) {
-    return res.status(400).json({ error: "No file uploaded" });
-  }
+// export const uploadTestService = async (req: Request, res: Response) => {
+//   if (!req.file) {
+//     return res.status(400).json({ error: "No file uploaded" });
+//   }
 
-  const { originalname, mimetype, size, buffer } = req.file;
+//   const { originalname, mimetype, size, buffer } = req.file;
 
-  const base64 = buffer.toString("base64");
+//   const base64 = buffer.toString("base64");
 
-  return res.json({
-    originalname,
-    mimetype,
-    size,
-    base64
-  });
-}
+//   return res.json({
+//     originalname,
+//     mimetype,
+//     size,
+//     base64
+//   });
+// }
